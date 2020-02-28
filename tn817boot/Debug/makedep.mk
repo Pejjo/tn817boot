1ab2f960dab70f25f642ead8897a0697457ebe36
Pejjo <per@hoj.nu>
2020-02-28 09:06:40 +0100

    Initial commit

---
+++ tn817boot/Debug/makedep.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+bootloader.c
+
+device_specific.c
+
+examples\src\nvmctrl_basic_example.c
+
+examples\src\usart_basic_example.c
+
+main.c
+
+src\bod.c
+
+src\nvmctrl_basic.c
+
+src\protected_io.S
+
+src\slpctrl.c
+
+src\usart_basic.c
+
+startup.c
+
