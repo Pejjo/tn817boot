17664bdbc842d9db3f45e6f28256ea057f362831
Pejjo <per@hoj.nu>
2020-02-28 09:08:43 +0100

    Revert "Initial commit"
    This reverts commit 1ab2f960dab70f25f642ead8897a0697457ebe36.

--- tn817boot/eeprom_map.h
+++
@@ -1,9 +0,0 @@
-#ifndef EEPROM_MAP_INCLUDE
-#define EEPROM_MAP_INCLUDE
-
-#include "avr/io.h"
-
-#define EEAD_CRIS USER_SIGNATURES_START-EEPROM_START+0 // This ends up in -100
-#define EEAD_SKLD USER_SIGNATURES_START-EEPROM_START+1 // This ends up in -99
-
-#endif
